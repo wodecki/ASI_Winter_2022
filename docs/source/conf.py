2d353df89451c606cb47d7778b51d0a62ad0b863
Andrzej Wodecki <andrzej.wodecki@gmail.com>
2022-10-31 13:02:58 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
+project = 'ASI Winter 2022'
+copyright = '2022, Andrzej Wodecki'
 author = 'Graziella'
 
 release = '0.1'
